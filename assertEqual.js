15639843bc95e0dff900c482531dd01efe0f5963
Sean Lee <lee.sean.cw@gmail.com>
2022-08-11 19:32:36 +0000

    wrote code in assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("Assertion Passed: " + actual + " === " + expected);
+  }
+  console.assert(actual === expected, actual + " !== " + expected);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
