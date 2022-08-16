70b19ccc2c2a3eefa72107fd083ac064420e337a
Sean Lee <lee.sean.cw@gmail.com>
2022-08-16 19:26:06 +0000

    added for loop in countLetters.js

--- countLetters.js
+++ countLetters.js
@@ -6,3 +6,15 @@ const assertEqual = function(actual, expected) {
     console.log(`Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+const countLetters = function(sentence) {
+  const results = {};
+
+  for (const letter of sentence) {
+    if (!results[letter]){
+      results[letter] = 1;
+    } else {
+      results[letter] += 1;
+    }
+  }
+}
