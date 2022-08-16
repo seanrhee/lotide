50bc8e671d4cf6a715b27cd58c91776495c5ca85
Sean Lee <lee.sean.cw@gmail.com>
2022-08-16 19:28:57 +0000

    added fix to skip spaces in input in countLetters.js

--- countLetters.js
+++ countLetters.js
@@ -11,10 +11,12 @@ const countLetters = function(sentence) {
   const results = {};
 
   for (const letter of sentence) {
-    if (!results[letter]){
-      results[letter] = 1;
-    } else {
-      results[letter] += 1;
+    if (letter !== ' '){
+      if (!results[letter]){
+        results[letter] = 1;
+      } else {
+        results[letter] += 1;
+      }
     }
   }
 
