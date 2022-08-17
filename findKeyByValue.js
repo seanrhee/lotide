066b7b6337b37903c29301cea0e84cf935896934
Sean Lee <lee.sean.cw@gmail.com>
2022-08-17 01:27:28 +0000

    started findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,22 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  }else{
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(obj, val) {
+  
+}
+
+// test
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
