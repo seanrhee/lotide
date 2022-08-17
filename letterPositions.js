185bc94046b2db175c4f6c2d7b66ae4706eb33de
Sean Lee <lee.sean.cw@gmail.com>
2022-08-17 01:25:09 +0000

    added letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,20 @@
+const letterPositions = function(sentence) {
+  const results = {};
+  // logic to update results here
+  
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== ' '){
+      console.log(sentence[i]);
+      if(!results[sentence[i]]){
+        results[sentence[i]] = [];
+        results[sentence[i]].push(i);
+      } else {
+        results[sentence[i]].push(i);
+      }
+    }
+  }
+
+  return results;
+};
+
+console.log(letterPositions("lighthouse in the house"));
