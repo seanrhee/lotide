ba42645c807048707908d999d78080aa09b661b7
Sean Lee <lee.sean.cw@gmail.com>
2022-08-17 21:54:48 +0000

    added assertObjectsEqual.js

---
+++ assertObjectsEqual.js
@@ -0,0 +1,33 @@
+const eqObjects = function(object1, object2) {
+  // check if objects have same # of keys
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;  
+  }
+
+  for (const key of Object.keys(object1)) {
+    if (Array.isArray(object1[key])){
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else {
+      if (object1[key] !== object2[key]) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+};
+
+const assertObjectsEqual = function(obj1, obj2) {
+
+  const inspect = require('util').inspect;
+
+  if (eqObjects(obj1, obj2)) {
+    console.log(`Assertion Passed: ${inspect(obj1)} === ${inspect(obj2)}`);
+  } else {
+    console.log(`Assertion Failed: ${inspect(obj1)} === ${inspect(obj2)}`);
+  }
+};
+
+// assertObjectsEqual({a: 1, c: 2}, {a: 1, c: 2});
--- eqArrays.js
+++ eqArrays.js
@@ -42,4 +42,4 @@ console.log(eqArrays([1, 2, 3], [3, 2, 3])); // => false
 console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
 console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
 
-assertEqual(eqArrays([1, 2, 3], [1, 3, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
