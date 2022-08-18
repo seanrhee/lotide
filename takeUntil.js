df7b936757b203fa8dc03ab213c23daeacdb9382
Sean Lee <lee.sean.cw@gmail.com>
2022-08-18 03:58:33 +0000

    added takeUntil.js

---
+++ takeUntil.js
@@ -0,0 +1,22 @@
+const takeUntil = function(array, callback) {
+  const output = [];
+  for (const item of array) {
+    console.log(item);
+    if(!callback(item)) {
+      output.push(item);
+    } else {
+      break;
+    }
+  }
+  return output;
+}
+
+// const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+// const results1 = takeUntil(data1, x => x < 0);
+// console.log(results1);
+
+// console.log('---');
+
+// const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+// const results2 = takeUntil(data2, x => x === 'been');
+// console.log(results2);
