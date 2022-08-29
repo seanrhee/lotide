5191f348a5970b95e5a04af5e6a581609acef3af
Sean Lee <lee.sean.cw@gmail.com>
2022-08-29 03:47:29 +0000

    mocha-fied headTest.js

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,12 @@
-// import head.js
-const head = require('../head');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-// TEST
-assertEqual(head([1,2,3]), 1);
-assertEqual(head(["Lighthouse", "Labs"]), "Hello");
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+});
